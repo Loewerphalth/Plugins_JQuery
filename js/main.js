06aae5bdb2ba2724e8c9ba0698d1d14c37b46464
Loewerphalth <Loewerphalth@gmail.com>
2025-04-28 23:27:23 -0300

    Primeiro commit do projeto com correções

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Corrigir as máscaras
 $('#telefone').mask('(00) 00000-0000', {
     placeholder: '(DDD) 12345-6789'
@@ -12,6 +13,20 @@ $('#cep').mask('00000-000', {
 });
 
 // Corrigir a validação
+=======
+$('#telefone').mask('(00) 00000-0000', {
+    placeholder: '(DDD) 12345-6789'
+})
+
+$('#cpf').mask('000.000.000-00', {
+    placeholder: '123.456.789-00'
+})
+
+$('#cep').mask('00000-000', {
+    placeholder: '012345-678'
+})
+
+>>>>>>> 59cad0c0857126c0b6cee3a78e42dc2fd2a69e84
 $('form').validate({
     rules: {
         nome: {
@@ -32,7 +47,11 @@ $('form').validate({
         },
         cpf: {
             required: true
+<<<<<<< HEAD
         }
+=======
+        },
+>>>>>>> 59cad0c0857126c0b6cee3a78e42dc2fd2a69e84
     },
     submitHandler: function (form) {
         alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
@@ -41,4 +60,8 @@ $('form').validate({
     invalidHandler: function (form, validator) {
         alert("Por favor, preencha os campos para prosseguir com a compra!");
     }
+<<<<<<< HEAD
 });
+=======
+})
+>>>>>>> 59cad0c0857126c0b6cee3a78e42dc2fd2a69e84
